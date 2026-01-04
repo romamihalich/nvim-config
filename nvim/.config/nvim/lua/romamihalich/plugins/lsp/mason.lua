e0f6e0324dd5db894cca8ece00faf10768d29112
romamihalich <miheev.roma90@mail.ru>
2026-01-04 22:18:04 +0300

    update plugins

--- nvim/.config/nvim/lua/romamihalich/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/romamihalich/plugins/lsp/mason.lua
@@ -9,29 +9,19 @@ require("mason-lspconfig").setup {
     ensure_installed = { },
 }
 
-require("mason-lspconfig").setup_handlers {
-    function (server_name)
-        local opts = {
-            capabilities = require("romamihalich.plugins.lsp.handlers").capabilities,
+-- disable, because already setup by plugin mrcjkb/rustaceanvim
+vim.lsp.enable("rust_analyzer", false)
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
         }
+    }
+})
 
-        if server_name == "rust_analyzer" then
-            -- already setup by plugin mrcjkb/rustaceanvim
-            return
-        end
-
-        if server_name == "lua_ls" then
-            opts = vim.tbl_deep_extend("force", {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" }
-                        }
-                    }
-                }
-            }, opts)
-        end
-
-        require("lspconfig")[server_name].setup(opts)
-    end,
-}
+vim.lsp.config("*", {
+    capabilities = require("romamihalich.plugins.lsp.handlers").capabilities,
+})
