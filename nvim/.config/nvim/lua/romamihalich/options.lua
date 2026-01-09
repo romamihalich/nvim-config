e174e5645b427ee9d549c38850d051b7ea2888be
romamihalich <miheev.roma90@mail.ru>
2026-01-09 23:05:18 +0300

    inline options

--- nvim/.config/nvim/lua/romamihalich/options.lua
+++ nvim/.config/nvim/lua/romamihalich/options.lua
@@ -1,41 +1,35 @@
-local options = {
-    clipboard = "unnamedplus",
-    tabstop = 4,
-    shiftwidth = 4,
-    expandtab = true,
-    smartindent = true,
-    relativenumber = true,
-    nu = true,
-    hidden = true,
-    errorbells = false,
-    wrap = false,
-    smartcase = true,
-    ignorecase = true,
-    hlsearch = true,
-    scrolloff = 8,
-    sidescrolloff = 8,
-    swapfile = false,
-    backup = false,
-    signcolumn= "yes",
-    mouse= "a",
-    timeoutlen = 250,
-    splitright = true,
-    splitbelow = true,
-    termguicolors = true,
-    undofile = true,
-    langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz",
-    wildignore = "node_modules/*,bin/*,obj/*,.git/*",
-    cursorline = true,
-    grepprg = "rg --vimgrep --no-heading",
-    grepformat = "%f:%l:%c:%m,%f:%l:%m,%f",
-    virtualedit = "block", -- Allow cursor to move where there is no text in visual block mode
-    listchars="tab:>·,trail:·,space:·,lead:·",
-    list = true,
-}
-
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.relativenumber = true
+vim.opt.nu = true
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.wrap = false
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.signcolumn= "yes"
+vim.opt.mouse= "a"
+vim.opt.timeoutlen = 250
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+vim.opt.wildignore = "node_modules/*,bin/*,obj/*,.git/*"
+vim.opt.cursorline = true
+vim.opt.grepprg = "rg --vimgrep --no-heading"
+vim.opt.grepformat = "%f:%l:%c:%m,%f:%l:%m,%f"
+vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+vim.opt.listchars="tab:>·,trail:·,space:·,lead:·"
+vim.opt.list = true
 
 vim.opt.iskeyword:append('-')
 vim.opt.diffopt:append('iwhite')
