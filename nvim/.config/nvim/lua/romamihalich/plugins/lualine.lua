9d4f096ac52af38759ad1f24dfeb01f72fd33bcb
romamihalich <miheev.roma90@mail.ru>
2026-01-10 00:41:33 +0300

    refactor lualine config

--- nvim/.config/nvim/lua/romamihalich/plugins/lualine.lua
+++ nvim/.config/nvim/lua/romamihalich/plugins/lualine.lua
@@ -17,30 +17,6 @@ return {
         "bwpge/lualine-pretty-path",
     },
     config = function()
-        local lualine = require'lualine'
-
-        local diagnostics = function()
-            local msg = 'No Active Lsp'
-            local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-            local clients = vim.lsp.get_clients()
-            if next(clients) == nil then return msg end
-            for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                    return client.name
-                end
-            end
-            return msg
-        end
-
-        local encoding = function ()
-            local enc = vim.opt.fileencoding:get()
-            if enc == "utf-8" then
-                return ""
-            end
-            return enc
-        end
-
         local pretty_path = {
             "pretty_path",
             symbols = {
@@ -56,7 +32,58 @@ return {
             color_correction = "dynamic"
         }
 
-        lualine.setup {
+        local diagnostics = {
+            'diagnostics',
+            sources = { 'nvim_diagnostic' },
+            sections = { "error", "warn", "hint" },
+            symbols = {
+                error = Signs.error .. " ",
+                warn = Signs.warn .. " ",
+                hint = Signs.hint .. " ",
+            },
+            colored = true,
+            update_in_insert = false,
+            always_visible = false,
+        }
+
+        local client_name = {
+            icon = ' LSP:',
+            function()
+                local msg = 'No Active Lsp'
+                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                local clients = vim.lsp.get_clients()
+                if next(clients) == nil then return msg end
+                for _, client in ipairs(clients) do
+                    local filetypes = client.config.filetypes
+                    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                        return client.name
+                    end
+                end
+                return msg
+            end
+        }
+
+        local encoding = {
+            function ()
+                local enc = vim.opt.fileencoding:get()
+                if enc == "utf-8" then
+                    return ""
+                end
+                return enc
+            end
+        }
+
+        local fileformat = {
+            'fileformat',
+            symbols = {
+                -- unix = '', -- e712
+                unix = '',
+                dos = '',  -- e70f
+                mac = '',  -- e711
+            }
+        }
+
+        require("lualine").setup {
             options = {
                 icons_enabled = true,
                 theme = 'auto',
@@ -72,30 +99,10 @@ return {
                     navic,
                 },
                 lualine_x = {
-                    {
-                        'diagnostics',
-                        sources = { 'nvim_diagnostic' },
-                        sections = { "error", "warn", "hint" },
-                        symbols = {
-                            error = Signs.error .. " ",
-                            warn = Signs.warn .. " ",
-                            hint = Signs.hint .. " ",
-                        },
-                        colored = true,
-                        update_in_insert = false,
-                        always_visible = false,
-                    },
-                    {diagnostics, icon = ' LSP:'},
-                    {encoding},
-                    {
-                        'fileformat',
-                        symbols = {
-                            -- unix = '', -- e712
-                            unix = '',
-                            dos = '',  -- e70f
-                            mac = '',  -- e711
-                        }
-                    },
+                    diagnostics,
+                    client_name,
+                    encoding,
+                    fileformat,
                     'filetype',
                     'location',
                 },
